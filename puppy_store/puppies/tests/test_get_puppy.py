45fe9af60cbb5dc4e9326b4bba4d2cfbe6af9da4
krishna <krishna147575@st.jmi.ac.in>
2019-04-25 01:03:02 +0530

    CRUD based RESTful API with Django Rest framework

--- puppy_store/puppies/tests/test_get_puppy.py
+++ puppy_store/puppies/tests/test_get_puppy.py
@@ -9,9 +9,8 @@ from ..serializers import PuppySerializer
 #initialize the API client app.
 client = Client()
 
-class GetAllPuppyTest(TestCase):
-
-    def SetUp(self):
+class GetSinglePuppyTest(TestCase):
+    def setUp(self):
         self.casper = Puppy.objects.create(
             name='Casper', age=3, breed='Bull Dog', color='Black')
         self.muffin = Puppy.objects.create(
@@ -21,10 +20,15 @@ class GetAllPuppyTest(TestCase):
         self.ricky = Puppy.objects.create(
             name='Ricky', age=6, breed='Labrador', color='Brown')
 
-    def test_get_all_puppies(self):
-        #get API response
-        response = client.get(reverse('get_post_puppies'))
-        puppies=Puppy.objects.all()
-        serializer = PuppySerializer(puppies, many=True)
+    def test_get_valid_single_puppy(self):
+        response = client.get(
+            reverse('get_delete_update_puppy', kwargs={'pk':self.rambo.pk}))
+        puppy = Puppy.objects.get(pk=self.rambo.pk)
+        serializer = PuppySerializer(puppy)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_puppy(self):
+        response = client.get(
+            reverse('get_delete_update_puppy', kwargs={'pk':30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
